e4f667a01797c9a90ee525d95e9fce8daa4eb31b
Ben <benshanahan1@gmail.com>
2017-09-27 20:24:29 -0400

    ground unit nonblocking read

--- carlson2/ground.py
+++ carlson2/ground.py
@@ -1,8 +1,8 @@
-import serial, struct
+import serial, struct, time
 
-s = serial.Serial(port="/dev/ttyUSB0", baudrate=57600, timeout=60)
+s = serial.Serial(port="/dev/ttyUSB0", baudrate=57600, timeout=0)
 
-sample_rate = 100  # sample rate in Hz
+telem_rate  = 10  # telemetry read rate in Hz
 t           = 0  # time stamp (sample time (sec) = t/sample_rate)
 
 telem_data_struct      = "Iffff"
@@ -12,7 +12,9 @@ labels = ["time","roll","pitch","yaw","altitude"]
 
 while (True):
     bytestream = s.read(telem_data_struct_size)
-    telem_data = struct.unpack(telem_data_struct, bytestream)
-    for idx, val in enumerate(telem_data):
-        print labels[idx], val
-    print "\n"
+    if len(bytestream) == telem_data_struct_size:
+        telem_data = struct.unpack(telem_data_struct, bytestream)
+        for idx, val in enumerate(telem_data):
+            print labels[idx], val
+        print "\n"
+    time.sleep(1/telem_rate)
