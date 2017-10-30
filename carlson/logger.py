705259991b458022bc06861245656636fce22087
Ben <benshanahan1@gmail.com>
2017-10-29 22:19:28 -0400

    added DEBUG option to logger

--- carlson/logger.py
+++ carlson/logger.py
@@ -14,25 +14,35 @@ from datetime import datetime
 ROOT      = "/home/pi/explosive-bakery/carlson/"
 LOG_DIR   = ROOT + "log/"
 VIDEO_DIR = ROOT + "video/"
+DEBUG_DIR = ROOT + "debug/"
 EXT       = "csv"
 VIDEO_EXT = "h264"
+
+# Define constants for Logger write targets.
+LOG   = 0
+DEBUG = 1
 
 # Video-specific parameters
 CAPTURE_RES = (1920, 1080)  # in pixels
 
 class Logger:
 
-    def __init__(self, init_logfile=True, init_camera=True):
-        self.file           = None
-        self.filename       = None
+    def __init__(self, init_log=True, init_camera=True, init_debug=True):
+        self.log            = None  # Data log file descriptor
+        self.logfilename    = None
+        self.log_enabled    = False
+        self.debug          = None  # Debug file descriptor
+        self.debugfilename  = None
+        self.debug_enabled  = False
         self.camera         = None
         self.camera_enabled = False
-        if init_logfile: self._init_new_logfile()
-        if init_camera:  self._init_camera()
+        if init_log:    self.log_enabled    = self._init_new_log()
+        if init_debug:  self.debug_enabled  = self._init_new_debug()
+        if init_camera: self.camera_enabled = self._init_camera()
 
     def start_video(self):
         if self.camera_enabled: 
-            self.camera.start_recording(VIDEO_DIR + self.filename + "." + VIDEO_EXT)
+            self.camera.start_recording(VIDEO_DIR + self.logfilename + "." + VIDEO_EXT)
             print "Started video capture."
 
     def stop_video(self):
@@ -43,44 +53,84 @@ class Logger:
     # Write data to file. Data is specified as a list, and delimeter is used
     # to separate each data point when written. If flush is True, this function
     # immediately flushes the written data to file as well.
-    def write(self, data, delimeter=",", flush=True):
-        # Append each element in data list to a string
-        log_str = ""
-        n       = len(data)
-        for idx, val in enumerate(data):
-            log_str += ("%s" % val)
-            # Add delimeter or newline to end of string
-            if idx == (n-1):
-                log_str += "\n"
-            else:
-                log_str += delimeter + "\t"
-
+    #
+    # Target can be either LOG (0) or DEBUG (1). By default, target is LOG file.
+    def write(self, data, target=LOG, delimeter=",", flush=True):
         # Write to file and flush (if specified)
-        self.file.write(log_str)
-        if flush: self.file.flush()
+        if target == LOG:
+            # Append each element in data list to a string
+            output = ""
+            n      = len(data)
+            for idx, val in enumerate(data):
+                output += ("%s" % val)
+                # Add delimeter or newline to end of string
+                if idx == (n-1):
+                    output += "\n"
+                else:
+                    output += delimeter + "\t"
+            self.log.write(output)
+            if flush: self.log.flush()
+        elif target == DEBUG:
+            # Print debug message with timestamp
+            output = "[" + datetime.now().strftime("%T") + "] " + data + "\n"
+            self.debug.write(output)
+            if flush: self.debug.flush()
+        else:
+            print "Invalid target specified."
+            return False
+        return True  # success
 
-    # Stop logger. This consists of closing the open file descriptor and 
-    # stopping our video stream.
-    def stop_all(self):
-        # Flush and close file
-        self.file.flush()
-        self.file.close()
+    # Stop logger. This consists of log file, debug file, and video capture, if
+    # they are enabled.
+    def stop(self):
+        if self.log_enabled:
+            self.log.flush()
+            self.log.close()
+        if self.debug_enabled:
+            self.debug.flush()
+            self.debug.close()
+        if self.camera_enabled:
+            self.stop_video()
 
-        # Stop video capture
-        self.stop_video()
-    
-    def _init_new_logfile(self):
+    # Open file descriptor to new log file
+    def _init_new_log(self):
+        self.logfilename = self.__generate_filename(LOG_DIR);
+        self.log = open("%s%s.%s" % (LOG_DIR, self.logfilename, EXT), "a")
+        print "Created log file (%s.%s)." % (self.logfilename, EXT)
+        return True
+
+    # Open file descriptor to new debug file
+    def _init_new_debug(self):
+        self.debugfilename = self.__generate_filename(DEBUG_DIR);
+        self.debug = open("%s%s.%s" % (DEBUG_DIR, self.debugfilename, EXT), "a")
+        print "Created debug file (%s.%s)." % (self.debugfilename, EXT)
+        return True
+
+    # Configure camera on the Pi
+    def _init_camera(self):
+        try:
+            self.camera = PiCamera()
+            self.camera.resolution = CAPTURE_RES
+            print "Initialized camera to capture at %d*%d px." % CAPTURE_RES
+            return True
+        except:
+            print "Failed to initialize camera."
+            return False
+
+
+    # Generate new filename based on what already exists in specified directory
+    def __generate_filename(self, directory, extension=EXT):
         # For every file in the log directory ending with EXT extension, we 
         # parse the number at the start of the filename, and if it is larger
         # than the number we have saved in last_launch, we update last_launch
         # to this new value.
         last_launch = 0
-        for f in [x for x in os.listdir(LOG_DIR) if x.endswith(EXT)]:
+        for f in [x for x in os.listdir(directory) if x.endswith(extension)]:
             try:
                 launch_numb = int(f.split("_")[0])  # get number at start of file
                 last_launch = launch_numb if launch_numb > last_launch else last_launch
             except ValueError:
-                print "Warning: Invalid .%s file in logs folder (%s)" % (EXT, f)
+                print "Warning: Invalid .%s file in logs folder (%s)" % (extension, f)
                 return False
 
         # Next, we generate a new filename based of the current system time.
@@ -89,27 +139,14 @@ class Logger:
         # last internet contact (i.e. this date will be wrong most of the 
         # time unless you connect to WiFi).
         date          = datetime.now().strftime("%y-%m-%d")
-        self.filename = "%03d_%s" % (last_launch + 1, date)
+        new_file_name = "%03d_%s" % (last_launch + 1, date)
 
         # Generate the full save path and verify that this file does not 
         # already exist.
-        fullpath = "%s%s.%s" % (LOG_DIR, self.filename, EXT)
+        fullpath = "%s%s.%s" % (directory, new_file_name, extension)
         if os.path.exists(fullpath):
             print "Error: Log file already exists (%s)" % fullpath
+            # TODO: make this error more visible
             return False
-            
-        # Get a file descriptor for the new file
-        self.file = open("%s%s.%s" % (LOG_DIR, self.filename, EXT), "a")
-        print "Created log file (%s.%s)." % (self.filename, EXT)
-        return True
-
-    # Configure camera on the Pi
-    def _init_camera(self):
-        try:
-            self.camera = PiCamera()
-            self.camera.resolution = CAPTURE_RES
-            self.camera_enabled = True
-            print "Initialized camera to capture at %d*%d px." % CAPTURE_RES
-        except:
-            self.camera_enabled = False
-            print "Failed to initialize camera."
+        else:
+            return new_file_name
