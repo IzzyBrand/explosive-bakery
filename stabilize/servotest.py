7162f01f1d2d25fae8f56620eed1a9cbd79e059d
izzybrand <isaiah_brand@brown.edu>
2017-11-28 04:59:25 +0000

    added script testing IMU and servo integration. updated servotest to use global servod

--- stabilize/servotest.py
+++ stabilize/servotest.py
@@ -1,7 +1,8 @@
+import subprocess
 import os
 import time
 
-os.system('sudo ./servod --p1pins="11,13,15"')
+os.system('sudo servod --p1pins="11,13,15"')
 val = 1000
 while True:
     val = 2000 if val == 1000 else 1000
